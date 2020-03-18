d4d2a4940fa160f41946fd8e91b1914ebf88b6fb
kbh3307 <61855314+kbh3307@users.noreply.github.com>
2020-03-18 18:24:04 +0900

    다이얼 이동까지

--- app/src/main/java/kr/co/tjoeun/intenttest01/SecondActivity.java
+++ app/src/main/java/kr/co/tjoeun/intenttest01/SecondActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Binder;
 import android.os.Bundle;
 import android.view.View;
 
@@ -30,6 +32,13 @@ public class SecondActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
                 startActivityForResult(intent, 3);
+            }
+        });
+        binding.dialBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                startActivity(intent);
             }
         });
     }
